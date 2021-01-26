61863bafc80d97d338e7b7bb1dea8a8dd8c7af36
neonb <neonb@SurfaceLaptop3-daebalprime>
2021-01-26 23:54:30 +0900

    asd

--- src/Bronze/bj_11283/Main.java
+++ src/Bronze/bj_11283/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-//		System.out.println((char)(4146));
+////		System.out.println((char)(4146));
 		System.out.println((int)sc.next().charAt(0)-(int)'가'+1);
 		sc.close();
 	}
--- src/Bronze/bj_8437/Main.java
+++ src/Bronze/bj_8437/Main.java
@@ -1,14 +1,16 @@
 package Bronze.bj_8437;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = Integer.parseInt(sc.nextLine());
-		int b = Integer.parseInt(sc.nextLine());
-		System.out.println((a/2 + b/2) + "\n" + (a/2 - b/2));
+		BigInteger a = new BigInteger(sc.nextLine());
+		BigInteger b = new BigInteger(sc.nextLine());
+		BigInteger c = new BigInteger("2");
+		System.out.println(a.add(b).divide(c) + "\n" + a.subtract(b).divide(c));
 		sc.close();
 	}
 
